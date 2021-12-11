aa2b07c40b52b916b4dbeca41876531161c5fa97
Enrique Barajas <enriqbar76@gmail.com>
2021-12-11 12:12:20 -0600

    Segundo comit: acá ya debería de mostrar el marcador pero aún no funciona.

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, Dimensions, StyleSheet, Text, View } from 'react-native';
+import * as Location from'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [locacion, setLocacion] =useState({})
+
+  const buscarLocation = async () => {
+    const {status} = await Location.requestForegroundPermissionsAsync()
+    if (status !== 'granted') {
+      return Alert.alert('No hay permisos para la geolocalización')
+    }
+    const location = await Location.getCurrentPositionAsync({})
+    console.log(location)
+    setLocacion(location)
+  }
+  useEffect(() => {
+    buscarLocation()
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map} >
+        {
+          locacion.coords ?
+          <Marker 
+            coordinate={locacion.coords}
+            title="Mi hubicación"
+            description = 'Aquí estoy'
+          />
+          : null  
+        }
+            
+      </MapView>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +46,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
